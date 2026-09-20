709a25972a04a533e321137a6b88bf200169ffc7
main agent <agent.main@wex.local>
2026-09-20 11:10:38 +0200

    feat(inventory): let each bundle declare itself a holder of design system elements

--- src/WexampleSymfonyDesignSystemDemoBundle.php
+++ src/WexampleSymfonyDesignSystemDemoBundle.php
@@ -2,18 +2,24 @@
 
 namespace Wexample\SymfonyDesignSystemDemo;
 
+use Wexample\SymfonyDesignSystem\Interface\DesignSystemElementsBundleInterface;
 use Wexample\SymfonyHelpers\Class\AbstractBundle;
 use Wexample\SymfonyHelpers\Helper\BundleHelper;
 use Wexample\SymfonyHelpers\Interface\LoaderBundleInterface;
 use Wexample\SymfonyPseudocode\Interface\PseudocodeBundleInterface;
 
-class WexampleSymfonyDesignSystemDemoBundle extends AbstractBundle implements LoaderBundleInterface, PseudocodeBundleInterface
+class WexampleSymfonyDesignSystemDemoBundle extends AbstractBundle implements LoaderBundleInterface, PseudocodeBundleInterface, DesignSystemElementsBundleInterface
 {
     public static function getLoaderFrontPaths(): array
     {
         return [
             BundleHelper::getBundleCssAlias(static::class) => __DIR__ . '/../assets/',
         ];
+    }
+
+    public static function getDesignSystemElementsPath(): string
+    {
+        return __DIR__ . '/../assets/';
     }
 
     public static function getPseudocodeSourcePaths(): array
